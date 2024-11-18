6db5c86e8339a6b430327bebbaa4db10a670d067
Mike-Future <153629361+Mike-Future@users.noreply.github.com>
2024-11-18 20:15:52 +0100

    improved database

--- Expense Tracker/database.py
+++ Expense Tracker/database.py
@@ -1,17 +1,44 @@
 import sqlite3
 
-conn = sqlite3.connect('expense.db')
-cursor = conn.cursor()
+def setup_database():
+    conn = sqlite3.connect('expense.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        amount REAL NOT NULL,
+        category TEXT NOT NULL,
+        description TEXT,
+        date TEXT NOT NULL
+    )
+    ''')
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS expenses (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    amount REAL NOT NULL,
-    category TEXT NOT NULL,
-    description TEXT,
-    date TEXT NOT NULL
-)
-''')
+    conn.commit()
+    conn.close()
 
-conn.commit()
-conn.close()
+
+def add_expense_to_db(amount, category, description, date):
+    conn = sqlite3.connect('expense.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO expenses (amount, category, description, date) VALUES (?, ?, ?, ?)',
+                   (amount, category, description, date))
+    
+    conn.commit()
+    conn.close()
+
+
+def get_all_expenses():
+    conn = sqlite3.connect('expenses.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM expenses')
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+
+def delete_expense_by_id(expense_id):
+    conn = sqlite3.connect('expenses.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM expenses WHERE id = ?', (expense_id))
+    conn.commit()
+    conn.close()
